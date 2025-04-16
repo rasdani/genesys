1bc33402ee05d62893d032654634474ffd3c9ea8
rasdani <73563550+rasdani@users.noreply.github.com>
2025-04-16 21:36:47 +0000

    init score functions

--- src/genesys/verifiers/reasoning_gym_verifier.py
+++ src/genesys/verifiers/reasoning_gym_verifier.py
@@ -1,30 +1,28 @@
-from reasoning_gym.factory import create_dataset
+from reasoning_gym.factory import DATASETS, create_dataset
 
 from genesys.schemas import Response
 from genesys.verifiers.base_verifier import BaseVerifier
 
 
 class ReasoningGymVerifier(BaseVerifier):
+    """
+    Verifier for procedural datasets from reasoning gym.
+    https://github.com/open-thought/reasoning-gym
+    """
 
     def __init__(self):
-        # TODO: init all datasets
-        pass
+        del DATASETS["composite"]  # TODO: remove this once the composite dataset is fixed
+        self.score_answer_fns = { dataset_name: create_dataset(name=dataset_name, size=1).score_answer for dataset_name in DATASETS.keys() }
 
     def verify(self, result: Response):
         """
-        Required: this example verify function checks the length of the llm response
-        and rewards responses over a threshold specified in the dataset.
+        Evaluates the answer with the corresponding scoring function from reasoning gym.
 
-        The output should be a dict with a score from 0-1 and a 'verification_result_info' dict containing metadata
+        The score is a float between 0 and 1.
         """
 
-        # Example: Instantiate a dataset using its registered name
         dataset_name = result["source"]
         entry = {"answer": result["verification_info"]["ground_truth"], "metadata": result["metadata"]}
-        config_kwargs = {
-            "size": 1,  # Example configuration, replace with actual configuration parameters
-        }
 
-        rg_ds = create_dataset(name=dataset_name, **config_kwargs)
-        score = rg_ds.score_answer(answer=result["llm_response"], entry=entry)
+        score = self.score_answer_fns[dataset_name](answer=result["llm_response"], entry=entry)
         return dict(score=score, verification_result_info={})
