cd02496bb09b5738e1d8ab9497e8f8ffa54ebeee
rasdani <73563550+rasdani@users.noreply.github.com>
2025-05-04 22:18:01 +0000

    fix: typo in registry

--- src/genesys/verifiers/registry.py
+++ src/genesys/verifiers/registry.py
@@ -2,14 +2,12 @@ from genesys.verifiers.code_test_verifier import CodeVerifier
 from genesys.verifiers.math_verifier import MathVerifier
 from genesys.verifiers.llm_judge_verifier import LlmJudgeVerifier
 from genesys.verifiers.code_output_prediction_verifier import CodeUnderstandingVerifier
-from genesys.verifiers.reasoning_gym_verifier import ReasoningGymVerifier
-from genesys.verifiers.swe_fixer_verfier import SweFixerVerifier
+from genesys.verifiers.swe_fixer_verifier import SweFixerVerifier
 
 VERIFIER_REGISTRY = {
     "verifiable_code": CodeVerifier,
     "verifiable_math": MathVerifier,
     "llm_judgeable_groundtruth_similarity": LlmJudgeVerifier,
     "code_output_prediction": CodeUnderstandingVerifier,
-    "reasoning_gym": ReasoningGymVerifier,
     "swe_fixer": SweFixerVerifier,
 }
