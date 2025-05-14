c21a453607a46ba44eb3cfb5d4f9ae2635100a37
rasdani <73563550+rasdani@users.noreply.github.com>
2025-05-14 23:26:18 +0000

    add to registry

--- src/genesys/verifiers/registry.py
+++ src/genesys/verifiers/registry.py
@@ -2,10 +2,12 @@ from genesys.verifiers.code_test_verifier import CodeVerifier
 from genesys.verifiers.math_verifier import MathVerifier
 from genesys.verifiers.llm_judge_verifier import LlmJudgeVerifier
 from genesys.verifiers.code_output_prediction_verifier import CodeUnderstandingVerifier
+from genesys.verifiers.ifeval_verifier import IFEvalVerifier
 
 VERIFIER_REGISTRY = {
     "verifiable_code": CodeVerifier,
     "verifiable_math": MathVerifier,
     "llm_judgeable_groundtruth_similarity": LlmJudgeVerifier,
     "code_output_prediction": CodeUnderstandingVerifier,
+    "ifeval": IFEvalVerifier,
 }
